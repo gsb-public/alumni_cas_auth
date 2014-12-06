a2ec496aca64d9f2e8ba296401cc95f5c143d063
Greg Mercer <gmercer@stanford.edu>
2014-12-06 08:27:52 -0800

    WP-4741 Changes to look for the cas-login before taking action.

--- js/alumni_cas_auth.js
+++ js/alumni_cas_auth.js
@@ -2,21 +2,26 @@
 
 Drupal.behaviors.color = {
   attach: function (context, settings) {
-    console.log(settings);
-    //var query = window.location.search;
-    //console.log('qstring = ' + query);
-    var check_auth =
-      jQuery.getJSON( settings.alumni_cas_auth.path + "alumni-cas-auth-check-auth", function(data) {
-        console.log(data.authenication_status);
-        console.log(data);
-        console.log( "success" );
-      })
-      .fail(function() {
-        console.log( "error" );
-      })
-      .always(function() {
-        console.log( "finished" );
-      });
+    if (jQuery('.cas-login')) {
+      var pathname = window.location.pathname;
+      pathname = pathname.substring(1);
+      console.log('pathname = ' + pathname);
+      var login_href = jQuery('.cas-login').attr('href');
+      //jQuery('.cas-login').attr('href', login_href + '?goto=' + pathname);
+      // Check if user is authenticated with Cas
+      var check_auth =
+        jQuery.getJSON( settings.alumni_cas_auth.path + "alumni-cas-auth-check-auth", function(data) {
+          console.log(data.authenication_status);
+          console.log(data);
+          console.log( "success" );
+        })
+        .fail(function() {
+          console.log( "error" );
+        })
+        .always(function() {
+          console.log( "finished" );
+        });
+    }
   }
 };
 
