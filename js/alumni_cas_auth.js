7619d2605971308369d3121c91f04f7208144067
Greg Mercer <gmercer@stanford.edu>
2014-12-06 08:40:25 -0800

    WP-4741 Show/hide the login and logout links depending on the user's authentication state.

--- js/alumni_cas_auth.js
+++ js/alumni_cas_auth.js
@@ -3,23 +3,15 @@
 Drupal.behaviors.color = {
   attach: function (context, settings) {
     if (jQuery('.cas-login')) {
-      var pathname = window.location.pathname;
-      pathname = pathname.substring(1);
-      console.log('pathname = ' + pathname);
-      var login_href = jQuery('.cas-login').attr('href');
-      //jQuery('.cas-login').attr('href', login_href + '?goto=' + pathname);
       // Check if user is authenticated with Cas
       var check_auth =
         jQuery.getJSON( settings.alumni_cas_auth.path + "alumni-cas-auth-check-auth", function(data) {
-          console.log(data.authenication_status);
-          console.log(data);
-          console.log( "success" );
+          if (data.authenication_status) {
+            jQuery('.cas-login').hide();
+          }
         })
         .fail(function() {
-          console.log( "error" );
-        })
-        .always(function() {
-          console.log( "finished" );
+          jQuery('.cas-logout').hide();
         });
     }
   }
