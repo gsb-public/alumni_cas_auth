ca71799486ffb3e51801a5a1e7fa0788b55722d5
Greg Mercer <gmercer@stanford.edu>
2015-01-23 11:04:53 -0800

    WP-4857 Temporary approach for handling the login interaction checking. Still working with Donald on some final details.

--- js/alumni_cas_auth.js
+++ js/alumni_cas_auth.js
@@ -4,17 +4,33 @@ Drupal.behaviors.color = {
   attach: function (context, settings) {
     if (jQuery('.cas-login')) {
       // Check if user is authenticated with Cas
-      var check_auth =
-        jQuery.getJSON( settings.alumni_cas_auth.path + "alumni-cas-auth-check-auth", function(data) {
-          if (data.authenication_status) {
-            jQuery('.cas-login').hide();
-          }
-        })
-        .fail(function() {
-          jQuery('.cas-logout').hide();
-        });
+      jQuery.ajax({
+        url: "https://pgnet13.stanford.edu/cas/pgjsonp.jsp?callback=getLoginType",
+        // The name of the callback parameter
+        jsonp: "callback",
+        // Tell jQuery we're expecting JSONP
+        dataType: "jsonp",
+        data: {
+          format: "json"
+        },
+        // Work with the response
+        success: function(response) {
+          //console.log( response );
+        }
+      });
     }
   }
 };
 
 })(jQuery);
+
+function getLoginType(data) {
+  if (data.loginState == "REME") {
+    jQuery('.cas-login').hide();
+    console.log('hiding login');
+  }
+  else {
+    jQuery('.cas-logout').hide();
+    console.log('hiding logout');
+  }
+}
