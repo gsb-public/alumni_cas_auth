68592b36bbe610feea1fef9b0346d5914036ae58
Greg Mercer <gmercer@stanford.edu>
2015-01-26 09:31:27 -0800

    WP-4857 Changed to use the getLoginType ajax call.

--- js/alumni_cas_auth.js
+++ js/alumni_cas_auth.js
@@ -1,36 +1,26 @@
 (function ($) {
 
-Drupal.behaviors.color = {
+Drupal.behaviors.alumni_case_auth = {
   attach: function (context, settings) {
     if (jQuery('.cas-login')) {
       // Check if user is authenticated with Cas
-      jQuery.ajax({
-        url: "https://pgnet13.stanford.edu/cas/pgjsonp.jsp?callback=getLoginType",
-        // The name of the callback parameter
-        jsonp: "callback",
-        // Tell jQuery we're expecting JSONP
-        dataType: "jsonp",
-        data: {
-          format: "json"
-        },
-        // Work with the response
-        success: function(response) {
-          //console.log( response );
+      jQuery.getJSON( settings.alumni_cas_auth.cas_getLogin_type_url, function(data) {
+        console.log(data);
+        if (data.loginState == "NONE") {
+          jQuery('.cas-logout').hide();
+          console.log('hiding logout');
         }
+        else {
+          jQuery('.cas-login').hide();
+          console.log('hiding login');
+        }
+      })
+      .fail(function() {
+        jQuery('.cas-logout').hide();
+        console.log('hiding logout');
       });
     }
   }
 };
 
 })(jQuery);
-
-function getLoginType(data) {
-  if (data.loginState == "REME") {
-    jQuery('.cas-login').hide();
-    console.log('hiding login');
-  }
-  else {
-    jQuery('.cas-logout').hide();
-    console.log('hiding logout');
-  }
-}
