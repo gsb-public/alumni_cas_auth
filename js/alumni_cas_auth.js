b9c05a209816ce1c0c4ef05dc6b71a438a740bde
Greg Mercer <gmercer@stanford.edu>
2015-02-02 14:19:20 -0800

    WP-4857 Switched check for loginState to use 'CAS'

--- js/alumni_cas_auth.js
+++ js/alumni_cas_auth.js
@@ -6,13 +6,13 @@ Drupal.behaviors.alumni_case_auth = {
       // Check if user is authenticated with Cas
       jQuery.getJSON( settings.alumni_cas_auth.cas_getLogin_type_url, function(data) {
         console.log(data);
-        if (data.loginState == "NONE") {
-          jQuery('.cas-logout').hide();
-          console.log('hiding logout');
-        }
-        else {
+        if (data.loginState == "CAS") {
           jQuery('.cas-login').hide();
           console.log('hiding login');
+        }
+        else {
+          jQuery('.cas-logout').hide();
+          console.log('hiding logout');
         }
       })
       .fail(function() {
