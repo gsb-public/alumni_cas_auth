ec47fac5361e4cecfaee27545c44228dcd6b5dce
Greg Mercer <gmercer@stanford.edu>
2015-04-15 11:27:14 -0700

    WP-5275 Removed console.log debugging messages.

--- js/alumni_cas_auth.js
+++ js/alumni_cas_auth.js
@@ -5,19 +5,15 @@ Drupal.behaviors.alumni_case_auth = {
     if (jQuery('.cas-login').length > 0) {
       // Check if user is authenticated with Cas
       jQuery.getJSON( settings.alumni_cas_auth.cas_getLogin_type_url, function(data) {
-        console.log(data);
         if (data.loginState == "CAS") {
           jQuery('.cas-login').hide();
-          console.log('hiding login');
         }
         else {
           jQuery('.cas-logout').hide();
-          console.log('hiding logout');
         }
       })
       .fail(function() {
         jQuery('.cas-logout').hide();
-        console.log('hiding logout');
       });
     }
   }
